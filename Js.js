da5aaa6f6d44df0668ecd282d30b3663d810061d
Liana_Kolodzheieva@example,com <liana.kolodgeeva@gmail.com>
2024-03-27 10:41:11 +0100

    added projekt

---
+++ Js.js
@@ -0,0 +1,23 @@
+
+function handleSubmit(event) {
+
+    event.preventDefault();
+
+    var nameInput = document.getElementById('nameInput');
+    var output = document.getElementById('output');
+
+    var name = nameInput.value.trim();
+
+    if (name === "") {
+        alert("Pole pytania jest puste");
+        return; 
+    }
+
+    var content = "Napisałeś pytanie! Poczekaj na odpowiedź<br>Name: " + name;
+
+    output.innerHTML = content;
+}
+
+var form = document.getElementById('myForm');
+
+form.addEventListener('submit', handleSubmit);
